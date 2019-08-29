9b1c4ac14b297a360c3222f616ce77b50b213270
Sophia Viviano <sviviano@zagmail.gonzaga.edu>
2019-08-28 22:15:47 -0700

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Sophia Viviano on 8/28/19.
+//  Copyright © 2019 Sophia Viviano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
