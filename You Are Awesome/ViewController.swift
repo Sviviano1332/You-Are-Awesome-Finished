2426e0da24b7f4eb8032f520159ea8e8ce357e2e
Sophia Viviano <sviviano@zagmail.gonzaga.edu>
2019-09-10 14:20:23 -0700

    2.12 Random Element

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    var index = 0
     
     //code below executes when the app's view first loads
     override func viewDidLoad() {
@@ -22,17 +23,38 @@ class ViewController: UIViewController {
 
     @IBAction func showMessagePresses(_ sender: UIButton) {
         
-        let message1 = "You Are Awesome!"
-        let message2 = "You Are Great!"
-        let message3 = "You Are Amazing!"
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the Genius Bar needs help, they call you",
+                        "You Brighten My Day!",
+                        "You are da bomb!",
+                        "Hey Fabulous!",
+                        "You Are Tremendous!",
+                        "You have the design skills of Johnny Ive!",
+                        "I cant wait to download your app!"]
         
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
-        } else {
-            messageLabel.text = message1
-        }
+        messageLabel.text = messages.randomElement()!
+        
+//        messageLabel.text = messages[index]
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
+        
+//        let message1 = "You Are Awesome!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
     }
 }
 
