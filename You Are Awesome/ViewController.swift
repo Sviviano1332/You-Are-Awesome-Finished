5c51eef46c5ae1001813595754355dbe3e3af87d
Sophia Viviano <sviviano@zagmail.gonzaga.edu>
2019-09-12 13:36:47 -0500

    2.19 More with Functions

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -35,14 +35,13 @@ class ViewController: UIViewController {
         return newIndex
     }
 
-    func playSound(soundName: String) {
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
         //can we load the file soundName?
         if let sound = NSDataAsset(name: soundName) {
             //check if sound.data is a sound file
-            
             do {
-                try awesomePlayer = AVAudioPlayer(data: sound.data)
-                awesomePlayer.play()
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
             } catch {
                 //if sound.data is not a valid audio file
                 print("ERROR: data in \(soundName) couldn't be played as a sound.")
@@ -82,7 +81,7 @@ class ViewController: UIViewController {
         
         // play a sound
         let soundName = "sound\(soundIndex)"
-        playSound(soundName: soundName)
+        playSound(soundName: soundName, audioPlayer: &awesomePlayer)
     }
 }
 
